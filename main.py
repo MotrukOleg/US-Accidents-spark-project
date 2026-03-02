e56d7fe18e4a1c9a42e9e2a84f38c0b1d85b255c
Ihor Yatsyshyn <ihoryatsyshyn4@gmail.com>
2026-03-03 00:18:43 +0200

    feat: created PySpark schemas (#5)

--- main.py
+++ main.py
@@ -2,20 +2,41 @@ import os
 import sys
 from pyspark.sql import SparkSession
 
+from schemas.raw_schema import raw_schema
+from config import DATA_PATH
+
+
 def main():
     os.environ['PYSPARK_PYTHON'] = sys.executable
     os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
 
-    spark = SparkSession.builder \
-        .appName("US-Accidents-Project") \
-        .config("spark.driver.host", "localhost") \
+    spark = (
+        SparkSession.builder
+        .appName("US-Accidents-Project")
+        .config("spark.driver.host", "localhost")
         .getOrCreate()
+    )
 
-    data = [("Project", "Started"), ("File", "Active")]
-    df = spark.createDataFrame(data, ["Entity", "Status"])
+    print("\n--- Spark Session успішно ініціалізована ---\n")
 
-    print("--- Spark Session успішно ініціалізована ---")
-    df.show() 
+    df = (
+        spark.read
+        .schema(raw_schema)
+        .option("header", True)
+        .csv(DATA_PATH)
+    )
+
+    print("=== SCHEMA ===")
+    df.printSchema()
+
+    print("\n=== FIRST 5 ROWS ===")
+    df.show(5, truncate=False)
+
+    print("\n=== ROW COUNT ===")
+    print(df.count())
+
+    spark.stop()
+
 
 if __name__ == "__main__":
     main()
