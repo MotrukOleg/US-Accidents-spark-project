86256191775db204c2e2f87d9886001fd507fd04
yurii-hr <yurii.hrabovskyi.shi.2023@lpnu.ua>
2026-03-14 22:13:47 +0200

    add parsing and transformation features

---
+++ modules/parsing.py
@@ -0,0 +1,18 @@
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col, year, month, dayofweek, hour, unix_timestamp, round, substring
+
+def parse_and_transform_features(df: DataFrame) -> DataFrame:
+
+    df_parsed = df \
+        .withColumn("Year", year(col("Start_Time"))) \
+        .withColumn("Month", month(col("Start_Time"))) \
+        .withColumn("DayOfWeek", dayofweek(col("Start_Time"))) \
+        .withColumn("Hour", hour(col("Start_Time")))
+    
+    duration_seconds = unix_timestamp(col("End_Time")) - unix_timestamp(col("Start_Time"))
+    df_parsed = df_parsed.withColumn("Duration_Minutes", round(duration_seconds / 60, 2))
+
+    df_parsed = df_parsed.withColumn("Zipcode_Base", substring(col("Zipcode"), 1, 5))
+    df_parsed = df_parsed.drop("Zipcode")
+    
+    return df_parsed
