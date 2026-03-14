85ba802967c486922a7607368e8f9ff00e48d3cd
Ihor Yatsyshyn <ihoryatsyshyn4@gmail.com>
2026-03-14 13:09:57 +0200

    FEAT: Created function for numerical features analysis (#9)
    * feat: created function for numerical features analysis
    
    * feat: added graphs

--- modules/eda_stats.py
+++ modules/eda_stats.py
@@ -2,6 +2,7 @@ import os
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pyspark.sql.functions as F
+from pyspark.sql.types import NumericType
 from config import OUTPUT_PLOT_DIR
 
 
@@ -45,3 +46,61 @@ def run_categorical_eda(df, categorical_cols):
 
             if not pd_df.empty:
                 plot_categorical_feature(pd_df, col_name)
+
+
+def get_numerical_stats(df, col_name):
+    stats_df = df.select(
+        F.count(F.col(col_name)).alias("count"),
+        F.count(F.when(F.col(col_name).isNull(), 1)).alias("nulls"),
+        F.mean(F.col(col_name)).alias("mean"),
+        F.stddev(F.col(col_name)).alias("stddev"),
+        F.min(F.col(col_name)).alias("min"),
+        F.expr(f"percentile_approx(`{col_name}`, 0.25)").alias("q1"),
+        F.expr(f"percentile_approx(`{col_name}`, 0.5)").alias("median"),
+        F.expr(f"percentile_approx(`{col_name}`, 0.75)").alias("q3"),
+        F.max(F.col(col_name)).alias("max")
+    )
+
+    return stats_df
+
+
+def run_numerical_eda(df, numerical_cols):
+    print("\n--- Статистика числових ознак ---")
+
+    for col_name in numerical_cols:
+        if col_name in df.columns:
+            print(f"\n--- Аналіз: {col_name} ---")
+
+            stats_df = get_numerical_stats(df, col_name)
+            stats_df.show(truncate=False)
+
+
+def plot_numerical_feature(df, col_name):
+    os.makedirs(OUTPUT_PLOT_DIR, exist_ok=True)
+
+    pd_df = df.select(col_name).dropna().toPandas()
+
+    if not pd_df.empty:
+        lower_bound = pd_df[col_name].quantile(0.01)
+        upper_bound = pd_df[col_name].quantile(0.99)
+
+        filtered_df = pd_df[
+            (pd_df[col_name] >= lower_bound) &
+            (pd_df[col_name] <= upper_bound)
+        ]
+
+        plt.figure(figsize=(10, 6))
+        sns.histplot(data=filtered_df, x=col_name, kde=True)
+        plt.title(f"Розподіл {col_name}")
+        plt.xlim(lower_bound, upper_bound)
+        plt.tight_layout()
+        plt.savefig(os.path.join(OUTPUT_PLOT_DIR, f"{col_name}_dist.png"))
+        plt.close()
+
+
+def run_numerical_plots(df, numerical_cols):
+    for col_name in numerical_cols:
+        if col_name in df.columns:
+            print(f"\n--- Графік: {col_name} ---")
+
+            plot_numerical_feature(df, col_name)
