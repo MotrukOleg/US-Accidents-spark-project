12c26ac3663e460eda41a62e74095ac79e45fff6
Ihor Yatsyshyn <ihoryatsyshyn4@gmail.com>
2026-03-24 00:19:00 +0200

    feat: created requests (#19)

--- analytics/transformation.py
+++ analytics/transformation.py
@@ -1,6 +1,8 @@
 from analytics.om_requests import om_requests
 from analytics.yv_requests import yv_requests
+from analytics.iy_queries import iy_queries
 
 def transform_data(olap):
     om_requests(olap)
     yv_requests(olap)
+    iy_queries(olap)
