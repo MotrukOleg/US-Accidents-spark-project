63ec279f72b2e82fc16be3d57492995e0bc4c403
Yana Vyklyuk <146078004+yanavyklyuk@users.noreply.github.com>
2026-03-24 14:24:17 +0200

    feat: add analytics queries and integrate into main pipeline (#21)
    * feat: add analytics queries and integrate into main pipeline
    
    * Update session configuration and improved code view
    
    ---------
    
    Co-authored-by: Olesia Yankiv <olesia.yankiv.shi.2023@lpnu.ua>

--- modules/olap.py
+++ modules/olap.py
@@ -8,10 +8,7 @@ from schemas.weather_dim_schema import weather_dim_schema
 from schemas.time_dim_schema import time_dim_schema
 from schemas.fact_schema import fact_schema
 
-
-
 def create_olap(df):
-
     road_cols = get_safe_cols(road_dim_schema, "RoadID", df)
 
     road_dim = (df.select(*road_cols)
